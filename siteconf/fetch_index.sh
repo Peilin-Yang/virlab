2fe74801322dc5d0e6dfcd50c81e6b80f2a0a087
Peilin Yang <yangpeilyn@gmail.com>
2017-02-02 10:16:54 -0500

    modify site setup scripts

--- siteconf/fetch_index.sh
+++ siteconf/fetch_index.sh
@@ -2,5 +2,4 @@
 
 wget https://s3.amazonaws.com/virlab/index.tar.gz
 tar xfz index.tar.gz
-rm index.tar.gz
-cp -r index /var/www/virlab/public_html/
+mv index /var/www/virlab/public_html/
--- siteconf/permission.sh
+++ siteconf/permission.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+mkdir -p /var/www/virlab/public_html/upload/
 chown -R $1 /var/www/virlab/public_html/upload/
 chmod -R 755 /var/www/virlab/public_html/upload/
 
+mkdir -p /var/www/virlab/public_html/users/
 chown -R $1 /var/www/virlab/public_html/users/
 chmod -R 755 /var/www/virlab/public_html/users/
 
+mkdir -p /var/www/virlab/public_html/out/
 chown -R $1 /var/www/virlab/public_html/out/
 chmod -R 755 /var/www/virlab/public_html/out/
